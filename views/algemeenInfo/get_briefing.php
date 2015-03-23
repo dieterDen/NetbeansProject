2c81f3bdc96285d7c5718eee8c972d8566be703a
dieterDen <dieter_denutte@hotmail.com>
2015-03-23 09:19:31 +0100

    briefing

--- views/algemeenInfo/get_briefing.php
+++ views/algemeenInfo/get_briefing.php
@@ -14,7 +14,7 @@
     $fi = new FilesystemIterator('external_files', FilesystemIterator::CURRENT_AS_PATHNAME);
     foreach ($fi as $fileinfo) {
         //verwijder 5min briefing pdf die ouder is dan een week
-        if ((abs(time() - $fi->getCTime()) / 60 / 60 / 24) > 7) {
+       /* if ((abs(time() - $fi->getCTime()) / 60 / 60 / 24) > 7) {
             unlink($fi->getFilename());
         }
         //echo (abs(time() - $fi->getCTime())/60/60/24);
@@ -30,9 +30,9 @@
         echo "succes";}
             else {
             echo 'failure';
-        }
+        }*/
       //  echo 'delete van 1903';
-        echo '<div title="5min briefing: ' . DATE("j/m/Y", filemtime($fi->current())) . '"data-options="""iconCls:icon-ok" style="overflow:auto;padding:10px;">'
+        echo '<div title="5min briefing: ' . DATE("j/m/Y", filectime($fi->current())) . '"data-options="""iconCls:icon-ok" style="overflow:auto;padding:10px;">'
         . '<p><a href=' . URL . 'external_files/' . $fi->getFilename() . ' style="font-size:14px;">' . $fi->getFilename() . '</a></p>'
         . '<form action="' . URL . 'algmeenInfo/get_briefingPDF" method="POST"><input class="easyio-linkbutton" name="delete" type="submit" value="Delete" id="Delete">'
         . '</form>'
