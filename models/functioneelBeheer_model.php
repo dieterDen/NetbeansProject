5c1dc894f22c4a81c67c15bb3b2a5a024783690b
dieterDen <dieter_denutte@hotmail.com>
2015-04-01 10:45:24 +0200

    regex imei

--- models/functioneelBeheer_model.php
+++ models/functioneelBeheer_model.php
@@ -18,38 +18,42 @@ class functioneelBeheer_model extends Model {
 
     /**
      * De functie haalt alle foute IMEI-nummers op voor de view get_imeiNummers.php
+     * Foutboodschap bepalen van oorzaak van fout IMEI-nummer
      * 
      * @return Array[][] Tweedimensionale array van key-value waarden van imei-nummers
      */
     function get_imeiNummers() {
-        //echo'inside model functioneel beheer';
         $result = $this->db->query("SELECT Dossiernaam as 'Dossiernummer', element as 'Element', elementnummer as 'Elementnummer',IMEINummer as 'IMEI-nummer' , merk as 'Merk'  from islp.view_imeiNummers ");
-        $row = $result->fetch_assoc();
-        if(preg_match("[$&+,:;=?@#|'<>.^*()%!-]",$row['IMEI-nummer'])){
-        $row['opmerking'] = "Geen IMEI-nummer opgegeven";}
-        else {
-          $row['opmerking'] = "Geen IMEI";
-                 }
-        print_r($ro);
         while ($row = $result->fetch_assoc()) {
             switch (true) {
-                case preg_match("[^a-zA-Z$]{+}",$row['IMEI-nummer']): $row['opmerking'] = "Geen speciale characters toegelaten";
-                case preg_match("[^a-zA-Z$]{}",$row['IMEI-nummer']): $row['opmerking'] = "Geen speciale characters toegelaten";
-                case empty($row['IMEI-nummer']): $row['opmerking'] = "Geen IMEI-nummer opgegeven, empty";
+                case preg_match('/^ *$/', $row['IMEI-nummer']): $row['opmerking'] = "Geen spaties toegelaten.";
                     break;
-                case is_null($row['IMEI-nummer']): $row['opmerking'] = "Geen IMEI-nummer opgegeven";
+                case empty($row['IMEI-nummer']): $row['opmerking'] = "Geen IMEI-nummer ingevuld";
+                    break;
+                case preg_match('/[^a-zA-Z\d]/', $row['IMEI-nummer']): $row['opmerking'] = "Geen speciale characters toegelaten";
+                    break;
+                case preg_match('/[a-zA-Z]/', $row['IMEI-nummer']): $row['opmerking'] = "Geen letters toegelaten.";
+                    break;
+                case preg_match('/^[\d]+$/', $row['IMEI-nummer']): $row['opmerking'] = "IMEI-nummer moet 14 cijfers lang zijn.";
+                    break;
+                case is_null($row['IMEI-nummer']): $row['opmerking'] = "Geen IMEI-nummer ingevuld";
                     break;
                 default : $row['opmerking'] = "OK";
             }
-
             switch (true) {
                 case empty($row['Merk']): $row['opmerkingMerk'] = "Geen merknaam ingevuld";
+                    break;
                 case is_null($row['Merk']): $row['opmerkingMerk'] = "Geen merknaam ingevuld";
+                    break;
+                case preg_match('/[^a-zA-Z\d]/', $row['Merk']):$row['opmerkingMerk'] = "Geen geldige merknaam";
+                    break;
+                case preg_match('/^[\d]*$/', $row['Merk']):$row['opmerkingMerk'] = "Geen geldige merknaam";
+                    break;
             }
 
             $rows[] = $row;
         }
-        print_r($rows);
+        //print_r($rows);
         return $rows;
     }
 
--- views/functioneelBeheer/get_imeiNummers.php
+++ views/functioneelBeheer/get_imeiNummers.php
@@ -20,17 +20,17 @@ include_once ("languages/ned_get_imeiNummers.php"); ?>
     <body>
         <div style="margin-bottom:20px">
         </div>
-        <table id="tt" class="easyui-datagrid" style="width:98%"
-               title="IMEI-nummer" fitColumns="true" data-options="collapsible:true">
+        <table id="tt" class="easyui-datagrid" style="width:98%" width="98%"
+               title="IMEI-nummer" data-options="singleSelect:true,collapsible:true">
             <thead>
                 <tr>
                     <th field="itemid" width="80" align="center"><?php echo $lang['dossiernummer']; ?></th>
-                    <th field="productid" width="120" align="center"><?php echo $lang['element']; ?></th>
-                    <th field="listprice" width="80" align="center"><?php echo $lang['elementnummer']; ?></th>
-                    <th field="imeinummer" width="80" align="center"><?php echo $lang['imeiNummer']; ?></th>
-                    <th field="opmerkingimei" width="80" align="center"><?php echo $lang['opmerkingImei']; ?></th>
+                    <th field="productid" width="350" align="center"><?php echo $lang['element']; ?></th>
+                    <th field="listprice" width="140" align="center"><?php echo $lang['elementnummer']; ?></th>
+                    <th field="imeinummer" width="120" align="center"><?php echo $lang['imeiNummer']; ?></th>
+                    <th field="opmerkingimei" width="400" align="center"><?php echo $lang['opmerkingImei']; ?></th>
                     <th field="attr1" width="150" align="center"><?php echo $lang['merk']; ?></th>
-                    <th field="opmerkingMerk" width="150" align="center"><?php echo $lang['OpmerkingMerk']; ?></th>
+                    <th field="opmerkingMerk" width="400" align="center"><?php echo $lang['OpmerkingMerk']; ?></th>
                 </tr>
             </thead>
             <tbody>
